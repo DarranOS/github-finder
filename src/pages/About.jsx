f14db1df6e02f88fd59a8a5e5dde7fb45390f5cc
DarranOS <darranoshea@gmail.com>
2022-01-18 19:44:05 +0000

    Finished Brad's Course
    No changes made to source material

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <div class>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+
+      <p>
+        A React app to search Gihub profiles and see profile details. This project is part
+        of the{' '}
+        <a href="https://www.udemy.com/course/modern-react-front-to-back">
+          {' '}
+          React Front To Back
+        </a>{' '}
+        Udemy course by <strong>Brad Traversy</strong>.
+      </p>
+
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0.</span>
+        Layout By: <span className="text-white">Hassib Moddasser</span>
+      </p>
+    </div>
+  )
+}
+
+export default About
